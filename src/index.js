0a2cfca33fefb48f2991a27a0dbbc225293cadce
misxka <mishutkavlesu@gmail.com>
2020-10-13 21:09:04 +0300

    feat: add reverse function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    while(n != 0) {
+        let surplus = n % 10;
+        n /= 10;
+        n = Math.trunc(n);
+        arr.push(surplus);
+    }
+    let reversedString = arr.join();
+    reversedString = reversedString.replace(/,?-|,/g, '');
+    let reversedInt = parseInt(reversedString);
+    return reversedInt;
 }
